c990606ea0a4a1afce0db8ecd155fb017cbb01ee
Thomas Leu <twelvestepsapp@gmail.com>
2014-05-12 13:31:42 +0800

    removed unused code from AADailyInventoryQuestionTableViewCell.m

--- Classes/AADailyInventoryQuestionTableViewCell.m
+++ Classes/AADailyInventoryQuestionTableViewCell.m
@@ -10,21 +10,6 @@
 
 @implementation AADailyInventoryQuestionTableViewCell
 
-- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
-{
-    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
-    if (self) {
-        // Initialization code
-    }
-    return self;
-}
-
-- (void)awakeFromNib
-{
-    // Initialization code
-}
-
-
 - (void)setQuestion:(AADailyInventoryQuestion *)question
 {
     _question = question;
