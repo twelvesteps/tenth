5990cc052fb6dfd7148e956a2d76f13512378742
Thomas Leu <twelvestepsapp@gmail.com>
2014-11-17 17:52:39 +0800

    Add AAMeetingViewController.h, add transitions to storyboard

---
+++ Classes/AAMeetingViewController.h
@@ -0,0 +1,13 @@
+//
+//  AAMeetingViewController.h
+//  Steps
+//
+//  Created by Tom on 11/17/14.
+//  Copyright (c) 2014 spitzgoby LLC. All rights reserved.
+//
+
+#import "AATableViewController.h"
+
+@interface AAMeetingViewController : AATableViewController
+
+@end
---
+++ Classes/AAMeetingViewController.m
@@ -0,0 +1,37 @@
+//
+//  AAMeetingViewController.m
+//  Steps
+//
+//  Created by Tom on 11/17/14.
+//  Copyright (c) 2014 spitzgoby LLC. All rights reserved.
+//
+
+#import "AAMeetingViewController.h"
+
+@interface AAMeetingViewController ()
+
+@end
+
+@implementation AAMeetingViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
