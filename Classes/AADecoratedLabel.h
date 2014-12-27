43f740547fb1aeea0407f790c5719d5b840dee65
Thomas Leu <twelvestepsapp@gmail.com>
2014-12-27 16:57:48 +0800

    Update comments

--- Classes/AADecoratedLabel.h
+++ Classes/AADecoratedLabel.h
@@ -8,28 +8,35 @@
 
 #import <UIKit/UIKit.h>
 
+/**
+ *  These constants determine the horizontal alignment of a decoration view
+ *  relative to adjacent text.
+ */
 typedef NS_ENUM(NSInteger, AADecorationAlignment) {
     /**
-     *  Decoration view is aligned to the left of the receiver's text
+     *  Decoration view is aligned to the left of the text
      */
     AADecorationAlignmentLeft,
     /**
-     *  Decoration view is aligned to the right of the receiver's text
+     *  Decoration view is aligned to the right of the text
      */
     AADecorationAlignmentRight,
 };
 
-typedef NS_ENUM(NSInteger, AATextAlignment) {
-    /**
-     *  Text is aligned to the left of the receiver's frame
+/**
+ *  These constants determine the horizontal alignment of text
+ */
+typedef NS_ENUM(NSInteger, AATextAlignment){
+    /** 
+     *  Text is aligned to the left of the frame
      */
     AATextAlignmentLeft,
-    /**
-     *  Text is aligned in the horizontal center of the receiver's frame
+    /** 
+     *  Text is aligned in the horizontal center of the frame
      */
     AATextAlignmentCenter,
-    /**
-     *  Text is aligned to the right of the receiver's frame
+    /** 
+     *  Text is aligned to the right of the frame
      */
     AATextAlignmentRight,
 };
@@ -37,10 +44,21 @@ typedef NS_ENUM(NSInteger, AATextAlignment) {
 
 @interface AADecoratedLabel : UIView
 
+/**-----------------------------------------------------------------------------
+ *  @name Setting the decoration view
+ *------------------------------------------------------------------------------
+ */
+
 /**
  *  The view to be displayed adjacent to the receiver's text
  */
 @property (nonatomic, weak) UIView* decorationView; // default is nil
+
+
+/**-----------------------------------------------------------------------------
+ *  @name Displaying text
+ *------------------------------------------------------------------------------
+ */
 
 /**
  *  The text to be displayed by the receiver
@@ -57,6 +75,11 @@ typedef NS_ENUM(NSInteger, AATextAlignment) {
  */
 @property (nonatomic, strong) UIColor* textColor; // default is [UIColor darkTextColor]
 
+
+/**-----------------------------------------------------------------------------
+ *  @name Laying out and aligning text and decoration view
+ *------------------------------------------------------------------------------
+ */
 /**
  *  Determines the horizontal insets of the decoration view and the 
  *  text from the receiver's frame. The top and bottom values are ignored.
