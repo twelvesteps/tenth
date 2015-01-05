1afa8b226cf4815dc13805c1d54bcd056d7cea4c
Thomas Leu <twelvestepsapp@gmail.com>
2015-01-05 09:28:24 +0800

    Remove synchronize calls from app delegate

--- Steps/AAAppDelegate.m
+++ Steps/AAAppDelegate.m
@@ -33,8 +33,6 @@
 {
     // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    [[AAUserContactsManager sharedManager] synchronize];
-    [[AAUserMeetingsManager sharedManager] synchronize];
     
     // This method is called when the user confirms a telprompt url
     [[AATelPromptDelegate sharedDelegate] fireTelPromptDidCall];
@@ -54,8 +52,6 @@
 - (void)applicationWillTerminate:(UIApplication *)application
 {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-    [[AAUserContactsManager sharedManager] synchronize];
-    [[AAUserMeetingsManager sharedManager] synchronize];
 } 
 
 @end
