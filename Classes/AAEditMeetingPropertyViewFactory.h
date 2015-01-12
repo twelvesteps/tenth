bef9270058457b98fc48e28fa83c39368ad53aa2
Thomas Leu <twelvestepsapp@gmail.com>
2015-01-12 18:55:34 +0800

    Add meeting property view factory, write interface

---
+++ Classes/AAEditMeetingPropertyViewFactory.h
@@ -0,0 +1,13 @@
+//
+//  AAEditMeetingPropertyViewFactory.h
+//  Steps
+//
+//  Created by Tom on 1/12/15.
+//  Copyright (c) 2015 spitzgoby LLC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AAEditMeetingPropertyViewFactory : NSObject
+
+@end
---
+++ Classes/AAEditMeetingPropertyViewFactory.m
@@ -0,0 +1,13 @@
+//
+//  AAEditMeetingPropertyViewFactory.m
+//  Steps
+//
+//  Created by Tom on 1/12/15.
+//  Copyright (c) 2015 spitzgoby LLC. All rights reserved.
+//
+
+#import "AAEditMeetingPropertyViewFactory.h"
+
+@implementation AAEditMeetingPropertyViewFactory
+
+@end
